35345d8fcc5a6434ddf19ec2325baaaea7a145d5
Chido100 <chido_dozie@hotmail.com>
2024-07-29 18:08:54 +0100

    Added posts urls

--- config/urls.py
+++ config/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path("api/v1/auth/", include("core_apps.users.urls")),
     path("api/v1/profiles/", include("core_apps.profiles.urls")),
     path("api/v1/ratings/", include("core_apps.ratings.urls")),
-    #path("api/v1/posts/", include("core_apps.posts.urls")),
+    path("api/v1/posts/", include("core_apps.posts.urls")),
 ]
 
 admin.site.site_header = "Minders Haven Admin"
