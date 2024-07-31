b47d454ada34eec6b55b852e84b417dda82b1b40
Chido100 <chido_dozie@hotmail.com>
2024-07-31 14:16:41 +0100

    setup fonts, eslint and prettier

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,10 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-6xl">Home Page</h1>
+      <h1 className="font-robotoSlab text-6xl">Home Page!!!</h1>
+      <p className="font-openSans text-3xl">
+        The quick brown fox jumps over the lazy dog.
+      </p>
     </div>
   );
 }
