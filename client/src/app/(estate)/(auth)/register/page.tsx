6f35317cd526735682be4beb41bd97f97ba61198
Chido100 <chido_dozie@hotmail.com>
2024-08-01 17:06:02 +0100

    Setup register page with validation

--- client/src/app/(estate)/(auth)/register/page.tsx
+++ client/src/app/(estate)/(auth)/register/page.tsx
@@ -1,13 +1,25 @@
-import { AuthFormHeader } from "@/components/forms/auth";
+"use client";
+import { AuthFormHeader, RegisterForm } from "@/components/forms/auth";
+// import OauthButtons from "@/components/shared/OauthButtons";
+// import { useRedirectIfAuthenticated } from "@/hooks";
 
 export default function RegisterPage() {
+	;
 	return (
 		<div>
-			<AuthFormHeader title="Sign up for an account" staticText="Already have an account?"
-			linkText="Login Here"
-			linkHref="/login"
+			<AuthFormHeader
+				title="Sign up for an account"
+				staticText="Already have an account?"
+				linkText="Login Here"
+				linkHref="/login"
 			/>
+			<div className="mt-7 sm:mx-auto sm:w-full sm:max-w-[480px]">
+				<div className="bg-lightGrey dark:bg-deepBlueGrey rounded-xl px-6 py-12 shadow sm:rounded-lg sm:px-12 md:rounded-3xl">
+					<RegisterForm />
+					
+					
+				</div>
+			</div>
 		</div>
 	);
 }
-
